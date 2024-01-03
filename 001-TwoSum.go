04716adf12aa7f0e67aa33a505ed4b60e4c34f65
AronGomu <neverismine@gmail.com>
2024-01-03 12:47:05 +0100

    cleaning 001

--- 001-TwoSum.go
+++ 001-TwoSum.go
@@ -1,7 +1,7 @@
-import "fmt"
+package main
 
 func main() {
-	twoSums([]int{3, 2, 4}, 6)
+	twoSum([]int{3, 2, 4}, 6)
 }
 
 func twoSum(nums []int, target int) []int {
@@ -11,16 +11,12 @@ func twoSum(nums []int, target int) []int {
 
 	for found != true {
 		for i := b; i < len(nums); i++ {
-			// fmt.Printf("a = %v, i = %v\n", a, i)
-			// fmt.Printf("%v + %v == %v => %v \n\n", nums[a], nums[i], target, nums[a]+nums[i] == target)
 			if nums[a]+nums[i] == target {
-				// fmt.Printf("is true \n")
 				return []int{a, i}
 			}
 		}
 		a += 1
 		b += 1
-		// fmt.Printf("a = %v, b = %v\n", a, b)
 
 		if b >= len(nums) {
 			found = true
